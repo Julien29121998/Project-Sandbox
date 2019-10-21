c95d419963f0898978e2e150a9015e71d0db3338
Julien29121998 <benardjr@gmail.com>
2019-10-21 18:22:05 +0200

    listé toutes les fonctions

--- functions.js
+++ functions.js
@@ -1,25 +1,63 @@
+
 function login(username, password){
 
 }
+
 function retrieveAllTests(){
 
 }
+
 function sendChosenTest(chosenTest){
 
 }
+
 function sendChosenLanguage(chosenLang){
 
 }
+
 function retrieveUserTests(username){
 
 }
+
 function retrieveAvLanguages(){
 
 }
+
 function showTask(test){
 
 }
+
 function compile(code){
 
 }
-/* not finished yet*/
+
+function submitcode(code){
+
+}
+
+function retrieveUser(username){
+    
+}
+
+function sendChosenUser(username){
+
+}
+
+function deleteTest(test){
+
+}
+
+function retrieveTemplate(template){
+
+}
+
+function sendTest(test){
+
+}
+
+function retrieveResults(session){
+
+}
+function logout(session){
+    
+}
